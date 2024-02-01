aa5ee9b1a4b75611dae3d8cc79042ed0cbe6a85c
ConsidineBlanche <137974116+ConsidineBlanche@users.noreply.github.com>
2024-01-31 23:40:00 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ chmod +x agent gost node cube .bedrock_server
 
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
-export UUID=""
+export UUID="1603eb55-848f-4a00-a97e-404f6b205976"
 export NEZHA_SERVER=""
 export NEZHA_PORT=""
 export NEZHA_KEY=""
